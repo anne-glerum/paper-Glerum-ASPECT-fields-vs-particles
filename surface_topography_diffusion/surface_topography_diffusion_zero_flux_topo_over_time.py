972893b50dc4d5d03818dace852de3326ac96922
anne-glerum <a.c.glerum@uu.nl>
2022-07-15 14:41:03 +0200

    Update zero flux pp script

--- surface_topography_diffusion/surface_topography_diffusion_zero_flux_topo_over_time.py
+++ surface_topography_diffusion/surface_topography_diffusion_zero_flux_topo_over_time.py
@@ -15,13 +15,41 @@ base = r"/Users/acglerum/Documents/Postdoc/SB_CRYSTALS/HLRN/HLRN/fix_stresses_el
 
 # Change file name modifiers as needed depending on your file structure
 names = [
-         "surface_topo_diffusion_zero_flux__dt0.0005_GR0",
+         "surface_topography_diffusion_zero_flux_dt0.002_GR4",
+         "surface_topography_diffusion_zero_flux_dt0.002_GR5",
+         "surface_topography_diffusion_zero_flux_dt0.002_GR6",
+         "surface_topography_diffusion_zero_flux_dt0.001_GR4",
+         "surface_topography_diffusion_zero_flux_dt0.001_GR5",
+         "surface_topography_diffusion_zero_flux_dt0.001_GR6",
+         "surface_topography_diffusion_zero_flux_dt0.0005_GR4",
+         "surface_topography_diffusion_zero_flux_dt0.0005_GR5",
+         "surface_topography_diffusion_zero_flux_dt0.0005_GR6",
         ]
 tail = r"/statistics"
+tail_topo = r"/topography"
 
 # The labels the graphs will get in the plot
 labels = [
-          'dt = 0.0005 s, dh = 1 m',
+          'dt = 0.002 s, dh = 0.0625 m',
+          'dt = 0.002 s, dh = 0.03125 m',
+          'dt = 0.002 s, dh = 0.015625 m',
+          'dt = 0.001 s, dh = 0.0625 m',
+          '',
+          '',
+          'dt = 0.0005 s, dh = 0.0625 m',
+          '',
+          '',
+         ]
+labels_error = [
+          '',
+          '',
+          '',
+          '',
+          'dt = 0.001 s, dh = 0.03125 m',
+          'dt = 0.001 s, dh = 0.015625 m',
+          '',
+          'dt = 0.0005 s, dh = 0.03125 m',
+          'dt = 0.0005 s, dh = 0.015625 m',
          ]
 # Set the colors available for plotting
 color1=[0.0051932, 0.098238, 0.34984]
@@ -30,51 +58,49 @@ color3=[0.32701, 0.4579, 0.28638]
 color4=[0.67824, 0.55071, 0.1778]
 color5=[0.97584, 0.63801, 0.50183]
 color6=[0.98447, 0.78462, 0.93553]
-colors = [color1, color2, color3, color4, color5, color5, color1, color3, color5]
+colors = [color1, color3, color5, color1, color3, color5, color1, color3, color5]
 # Set the line styles
-linestyles = ['solid', 'solid', 'solid', 'solid', 'solid', 'dotted', 'dotted', 'dotted', 'dotted'] 
+linestyles = ['solid', 'solid', 'solid', 'dashed', 'dashed', 'dashed', 'dotted', 'dotted', 'dotted'] 
 # Set the marker styles (no markers in this case)
 markers = ['', '', '', '', '', '', '', '', ''] 
 # Only plot every nth marker
-dmark=35
+dmark=15
 
-# Set up a row of two plots, one with absolute stress values
-# and one with the percentage difference to the analytical solution
-fig = plt.figure(figsize=(10, 6))
-ax = [fig.add_subplot(2, 1, i) for i in range(1, 3)]
+# Set up a column of three plots, one with the full topography over time,
+# one with the maximum topography over time (i.e. the topography at x = 0.5)
+# and one with the percentage difference to the analytical solution for the maximum topography at x = 0.5.
+fig = plt.figure(figsize=(10, 9))
+ax = [fig.add_subplot(3, 1, i) for i in range(1, 4)]
 
-# Also plot a zoom-in of the absolute stress values
+# Also plot a zoom-in of the topography
 zoom_x_min = 0.45
-zoom_x_max = 0.65
-zoom_y_min = 10
-zoom_y_max = 13
-#axins = zoomed_inset_axes(ax[0], 3, loc = "center") # zoom = 2
-#axins.set_xlim(zoom_x_min, zoom_x_max)
-#axins.set_ylim(zoom_y_min, zoom_y_max)
-#mark_inset(ax[0], axins, loc1=1, loc2=4, fc="none", ec="0.5")
+zoom_x_max = 0.50
+zoom_y_min = 0.057
+zoom_y_max = 0.061
+axins = zoomed_inset_axes(ax[0], 4, loc = "upper right") # zoom = 4
+axins.set_xlim(zoom_x_min, zoom_x_max)
+axins.set_ylim(zoom_y_min, zoom_y_max)
+mark_inset(ax[0], axins, loc1=1, loc2=4, fc="none", ec="0.5")
 
-yr_in_secs = 3600. * 24. * 365.2425
 counter = 0 
 
-# The analytical solution:
-# Return stress in Pa.
-def topo_analytical(x,time):
+# The analytical solution.
+# Return topography at the center x-coordinate in m.
+def topo_analytical(xc,time):
     topo = 0
     amplitude = 0.075
     domain_width = 1
-    nmax = 5000
+    n_max = 5000
     kappa = 0.25
-    if t <= 0:
-      topo = amplitude * np.sin(np.PI*x/domain_width)
-    else:
-      sum = 0.
-      for n in range(1, n_max+1):
-        sum += np.cos(2.*n*np.PI*x/domain_width)
-               * exp(-kappa*4.*n*n*np.PI*np.PI*time/(domain_width*domain_width))
-               / ((4.*n*n)-1.)
-               * (-4.*amplitude/np.PI);
-        # a0=4A/pi --> a0/2=2A/pi
-        topo = 2. * amplitude / np.PI + sum;
+    # Fill another array of the size of time
+    # with the x-coordinate so that x and time
+    # have the same dimension.
+    x = np.full((np.size(time)), xc)
+    sum = 0.
+    for n in range(1, n_max+1):
+      sum += np.cos(2.*n*np.pi*x/domain_width) * np.exp(-kappa*4.*n*n*np.pi*np.pi*time/(domain_width*domain_width)) / ((4.*n*n)-1.) * (-4.*amplitude/np.pi);
+      # a0=4A/pi --> a0/2=2A/pi
+    topo = 2. * amplitude / np.pi + sum;
 
     return topo
 
@@ -82,56 +108,106 @@ def topo_analytical(x,time):
 for name in names: 
   # Create file path
   path = base+name+tail
+  path_topo = base+name+tail_topo
 
-  # Read in the time and the minimum xx component of the viscoelastic stress,
-  # which is stored on the field ve_stress_xx.
+  # Read in the time and the maximum topography.
   # The correct columns are selected with usecols (counting starts from 0).
   if 'particle' in name:
-    time,topo = np.genfromtxt(path, comments='#', usecols=(1,12), unpack=True)
+    time,max_topo = np.genfromtxt(path, comments='#', usecols=(1,19), unpack=True)
   else:
-    time,topo = np.genfromtxt(path, comments='#', usecols=(1,24), unpack=True)
+    time,max_topo = np.genfromtxt(path, comments='#', usecols=(1,19), unpack=True)
 
-  # Plot the stress elements in Pa against time in s in
+  # Plot the topography at the center of the domain against time in s in
   # categorical batlow colors.
-  ax[0].plot(time,topo,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
-  #axins.plot(time,topo,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
-  ax[1].plot(time,(topo-topo_analytical(time))/topo_analytical(time)*100.,label=labels[counter],color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
-  
+  ax[1].plot(time,max_topo,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+  ax[2].plot(time,(max_topo-topo_analytical(0.5,time))/topo_analytical(0.5,time)*100.,label=labels_error[counter],color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+ 
+  if '0.002' in name:
+    x,topo = np.genfromtxt(path_topo+".00000", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain, subtract domain height
+    ax[0].plot(x,topo-1,label=labels[counter],color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+
+    x,topo = np.genfromtxt(path_topo+".00025", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain, subtract domain height
+    ax[0].plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+
+    x,topo = np.genfromtxt(path_topo+".00050", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain 
+    ax[0].plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+    axins.plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+  elif '0.001' in name:
+    x,topo = np.genfromtxt(path_topo+".00000", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain 
+    ax[0].plot(x,topo-1,label=labels[counter],color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+
+    x,topo = np.genfromtxt(path_topo+".00050", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain 
+    ax[0].plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+
+    x,topo = np.genfromtxt(path_topo+".00100", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain 
+    ax[0].plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+    axins.plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+  elif '0.0005' in name:
+    x,topo = np.genfromtxt(path_topo+".00000", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain 
+    ax[0].plot(x,topo-1,label=labels[counter],color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+
+    x,topo = np.genfromtxt(path_topo+".00100", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain 
+    ax[0].plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+
+    x,topo = np.genfromtxt(path_topo+".00200", comments='#', usecols=(0,1), unpack=True)
+    # Plot the topography along the surface of the domain 
+    ax[0].plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+    axins.plot(x,topo-1,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+  else:
+    print ("Timestep not accounted for")
+
   counter += 1
 
 # Plot the analytical solution in Pa.
-ax[0].plot(time,topo_analytical(time),label='analytical',color='black',linestyle='dashdot')
-#axins.plot(time,1e-6*tau_xy_analytical(time),label='analytical',color='black',linestyle='dashdot')
+ax[1].plot(time,topo_analytical(0.5,time),label='analytical',color='black',linestyle='dashdot')
+#axins.plot(time,topo_analytical(0.5,time),label='analytical',color='black',linestyle='dashdot')
 
 # Labelling of plot
-ax[1].set_xlabel("Time [s]")
+ax[0].set_xlabel("X [m]")
+ax[0].xaxis.set_label_coords(.5,-0.1)
 ax[0].set_ylabel(r"Topography [m]")
-ax[1].set_ylabel(r"Error [%]")
-# Manually place legend in lower right corner. 
-ax[0].legend(loc='lower right')
+ax[2].set_xlabel("Time [s]")
+ax[1].set_ylabel(r"Topography at x = 0.5 [m]")
+ax[2].set_ylabel(r"Error at x = 0.5 [%]")
+# Manually place legend 
+ax[0].legend(loc='lower center')
 ax[1].legend(loc='lower right')
+ax[2].legend(loc='upper left')
 # Grid and tickes
 ax[0].grid(axis='x',color='0.95')
-ax[0].set_yticks([0,5,10,15])
 ax[0].grid(axis='y',color='0.95')
+ax[0].set_yticks([0,0.02,0.04,0.06,0.08])
 ax[1].grid(axis='x',color='0.95')
 ax[1].grid(axis='y',color='0.95')
-#ax[1].set_yticks([0,0.1,0.2,0.3,0.4,0.5])
+ax[2].grid(axis='x',color='0.95')
+ax[2].grid(axis='y',color='0.95')
+#ax[2].set_yticks([0,0.1,0.2,0.3,0.4,0.5])
 
 # Ranges of the axes
-ax[0].set_xlim(0,0.1) # s
-#ax[0].set_ylim(0,15.5) # MPa
+ax[0].set_xlim(0,1) # s
+ax[0].set_ylim(0.0,0.08) # m
 ax[1].set_xlim(0,0.1) # s
-#ax[1].set_ylim(-0.5,0.5) # %
+ax[1].set_ylim(0.05,0.08) # m
+ax[2].set_xlim(0,0.1) # s
+#ax[2].set_ylim(-0.5,0.5) # %
 
 # Add labels a) and b)
-#ax[0].text(-16,208,"a)")
-#ax[1].text(-16,-0.05,"b)")
+ax[0].text(-0.07,0.078,"a)")
+ax[1].text(-0.0078,0.08,"b)")
+ax[2].text(-0.007,0.3,"c)")
 
 # Add timestep labels
-#ax[1].text(50,-0.77,"dt = 500 yr", rotation = 20)
-#ax[1].text(50,-0.30,"dt = 250 yr", rotation = 6)
-#ax[1].text(5,-0.1,"dt = 125 yr", rotation = 3)
+ax[0].text(0.027,0.001,"t = 0 s", rotation = 37)
+ax[0].text(0.021,0.021,"t = 0.5 s", rotation = 8)
+ax[0].text(0.025,0.039,"t = 1 s", rotation = 3)
 
 
 plt.tight_layout()
