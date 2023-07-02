64d77db9e737008a91f343e8704d0820022cbce0
anne-glerum <a.c.glerum@uu.nl>
2023-07-02 20:50:57 +0200

    Update shear torsion

--- viscoelastic_sheared_torsion/viscoelastic_sheared_torsion_dtc_isnot_dte_stress_xx_over_time.py
+++ viscoelastic_sheared_torsion/viscoelastic_sheared_torsion_dtc_isnot_dte_stress_xx_over_time.py
@@ -5,7 +5,7 @@ Created on Tue Sep 21 by Anne Glerum
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
-from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes 
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 rc("pdf", fonttype=42)
 rc("lines", linewidth=3, markersize=8)
@@ -15,17 +15,35 @@ base = r"/Users/acglerum/Documents/Postdoc/SB_CRYSTALS/HLRN/HLRN/fix_stresses_el
 
 # Change file name modifiers as needed depending on your file structure
 names = [
-         "ve_sheared_torsion_dtc0.01_dte0.01_GR0",
+#         "ve_sheared_torsion_dtc0.02_dte0.02_GR1",
+#         "ve_sheared_torsion_dtc0.01_dte0.02_GR1",
+#         "ve_sheared_torsion_dtc0.005_dte0.02_GR1",
          "ve_sheared_torsion_dtc0.01_dte0.01_GR1",
+#         "ve_sheared_torsion_dtc0.005_dte0.01_GR1",
+#         "ve_sheared_torsion_dtc0.0025_dte0.01_GR1",
+         "ST_test_2",
+         "ST_test_3",
+         "ST_test_4",
+         "ST_test_particles",
+#         "ST_test_particles_3",
         ]
 tail = r"/statistics"
 
 # The labels the graphs will get in the plot
 labels = [
-          'dtc = dte = 0.01 s, dh = 1 m',
-          'dtc = dte = 0.01 s, dh = 0.5 m',
-          'dtc = dte = 0.005 s',
-          'dtc = dte = 0.001 s',
+#          'dtc = 0.02 s, dte = 0.02 s, dh = 0.5 m',
+#          'dtc = 0.01 s, dte = 0.02 s, dh = 0.5 m',
+#          'dtc = 0.005 s, dte = 0.02 s, dh = 0.5 m',
+          'dtc = 0.01 s, dte = 0.01 s, dh = 0.5 m, original',
+          'dtc = 0.01 s, dte = 0.01 s, dh = 0.5 m',
+          'dtc = 0.005 s, dte = 0.005 s, dh = 0.5 m',
+          'dtc = 0.001 s, dte = 0.001 s, dh = 0.5 m',
+#          'dtc = 0.005 s, dte = 0.01 s, dh = 0.5 m',
+#          'dtc = 0.0025 s, dte = 0.01 s, dh = 0.5 m',
+#          'dtc = 0.01 s, dte = 0.01 s, dh = 0.5 m',
+          'dtc = 0.01 s, dte = 0.01 s, dh = 0.5 m, particles',
+          'dtc = 0.005 s, dte = 0.005 s, dh = 0.5 m, particles',
+          'dtc = 0.01 s, dte = 0.01 s, dh = 0.5 m, particles, no rot',
          ]
 # Set the colors available for plotting
 color1=[0.0051932, 0.098238, 0.34984]
@@ -34,11 +52,11 @@ color3=[0.32701, 0.4579, 0.28638]
 color4=[0.67824, 0.55071, 0.1778]
 color5=[0.97584, 0.63801, 0.50183]
 color6=[0.98447, 0.78462, 0.93553]
-colors = [color2, color3, color4, color5, color6, color5, color1, color3, color5]
+colors = [color1, color2, color3, color4, color5, color6, color1, color3, color5]
 # Set the line styles
-linestyles = ['solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'dotted', 'dotted', 'dotted'] 
+linestyles = ['solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'dotted', 'dotted', 'dotted']
 # Set the marker styles (no markers in this case)
-markers = ['', '', '', '', '', '', '', '', ''] 
+markers = ['', '', '', '', '', '', '', '', '']
 # Only plot every nth marker
 dmark=35
 
@@ -58,7 +76,7 @@ zoom_y_max = 13
 #mark_inset(ax[0], axins, loc1=1, loc2=4, fc="none", ec="0.5")
 
 yr_in_secs = 3600. * 24. * 365.2425
-counter = 0 
+counter = 0
 
 # The analytical solution:
 # Return stress in Pa.
@@ -68,7 +86,9 @@ def tau_xz_analytical(t):
     eta_v=1e2
     h=1
     C1 = -(V*V*eta_v*eta_v*mu)/(mu*mu*h*h+V*V*eta_v*eta_v)
+    #print ("C1", C1)
     C2 = -(V*h*eta_v*mu*mu)/(mu*mu*h*h+V*V*eta_v*eta_v)
+    #print ("C2", C2)
     # at 0.5, shearing stops
     tmax=0.5
     return np.where(t<=tmax, \
@@ -76,7 +96,7 @@ def tau_xz_analytical(t):
     (np.exp(-mu/eta_v*tmax)*(C2*np.cos(V*tmax/h)-C1*np.sin(V*tmax/h))-C2)*np.exp(-mu/eta_v*(t-tmax)))
 
 
-for name in names: 
+for name in names:
   # Create file path
   path = base+name+tail
 
@@ -84,20 +104,24 @@ for name in names:
   # which is stored on the field ve_stress_xz.
   # The correct columns are selected with usecols (counting starts from 0).
   if 'particle' in name:
-    time,stress_xz_min = np.genfromtxt(path, comments='#', usecols=(1,12), unpack=True)
+    time,stress_xz_min,stress_xz_max,stress_xz_global = np.genfromtxt(path, comments='#', usecols=(1,27,28,29), unpack=True)
   else:
-    time,stress_xz_min = np.genfromtxt(path, comments='#', usecols=(1,40), unpack=True)
+    time,stress_xz_min,stress_xz_max,stress_xz_global = np.genfromtxt(path, comments='#', usecols=(1,39,40,41), unpack=True)
+
+  print ("xz min, xz_max, xz_global: ", stress_xz_min,stress_xz_max,stress_xz_global)
 
   # Plot the stress elements in Pa against time in s in
   # categorical batlow colors.
-  ax[0].plot(time,stress_xz_min,label=labels[counter],color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+  #ax[0].plot(time,stress_xz_min,label=labels[counter],color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+  ax[0].plot(time,stress_xz_global,label=labels[counter],color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
   #axins.plot(time,stress_xz_min,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
-  ax[1].plot(time,(stress_xz_min-tau_xz_analytical(time))/tau_xz_analytical(time)*100.,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+  #ax[1].plot(time,(stress_xz_min-tau_xz_analytical(time))/tau_xz_analytical(time)*100.,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
+  ax[1].plot(time,(stress_xz_global-tau_xz_analytical(time))/tau_xz_analytical(time)*100.,label=None,color=colors[counter],linestyle=linestyles[counter],marker=markers[counter],markevery=dmark)
 
   for tau in stress_xz_min:
     if tau > 15:
       print ("Too high tau: ", name)
-  
+
   counter += 1
 
 # Plot the analytical solution in Pa.
@@ -108,7 +132,7 @@ ax[0].plot(time,tau_xz_analytical(time),label='analytical',color='black',linesty
 ax[1].set_xlabel("Time [s]")
 ax[0].set_ylabel(r"Viscoelastic stress $\tau0_{xz}$ [Pa]")
 ax[1].set_ylabel(r"Error [%]")
-# Manually place legend in lower right corner. 
+# Manually place legend in lower right corner.
 ax[0].legend(loc='lower center',handlelength=4)
 #ax[1].legend(loc='lower right',ncol=1,handlelength=4)
 # Grid and tickes
@@ -120,14 +144,14 @@ ax[1].grid(axis='y',color='0.95')
 #ax[1].set_yticks([0,0.1,0.2,0.3,0.4,0.5])
 
 # Ranges of the axes
-ax[0].set_xlim(0,1.) # s
+ax[0].set_xlim(0,1) # s
 ax[0].set_ylim(0,15.5) # MPa
 ax[1].set_xlim(0,1.) # s
-ax[1].set_ylim(-1.,0.75) # %
+#ax[1].set_ylim(-0.75,0.25) # %
 
 # Add labels a) and b)
 ax[0].text(-0.055,15.5,"a)")
-ax[1].text(-0.07,0.75,"b)")
+ax[1].text(-0.07,0.25,"b)")
 
 # Add timestep labels
 #ax[1].text(50,-0.77,"dt = 500 yr", rotation = 20)
@@ -138,4 +162,4 @@ ax[1].text(-0.07,0.75,"b)")
 plt.tight_layout()
 
 # Save
-plt.savefig('4_viscoelastic_sheared_torsion_dtc_isnot_dte_dtcisdte.png')    
+plt.savefig('4_viscoelastic_sheared_torsion_dtc_isnot_dte_dtcisdte_dtcisnotdte.png')
